8c581981dcac5ac99eae4f1ce30371ca718eca03
iconicnn <97861308+iconicnn@users.noreply.github.com>
2022-08-28 17:19:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,23 +1,22 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx3e63b6bf7a22f8df",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "63e7fcf97ec287461f634cca96db98e8",
     // 模板消息id
-    templateId: "",
+    templateId: "fk_4sbuZretyMuGxO2MoRk2qCPsC61ypywE-UUnlvIE",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: ["oUbJj6GzUToGXJNkbrsiDhGsPZLw"],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "河南",
     // 所在城市
-    city: "惠州",
+    city: "驻马店",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "老婆", "year": "1999", "date": "08-30", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2015-05-01",
