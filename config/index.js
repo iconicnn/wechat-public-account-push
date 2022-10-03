8d6ab945862946a20f387d4488a8b8e08bcd1636
iconicnn <97861308+iconicnn@users.noreply.github.com>
2022-10-03 22:33:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ export const config = {
     // 公众号appSecret
     appSecret: "63e7fcf97ec287461f634cca96db98e8",
     // 模板消息id
-    templateId: "dj-mhKAucrw7lBScKkDu-tOBvn5nmIzve1Q-hdRArNI",
+    templateId: "yN1Dq-URgHe0Ui2vSH4Nqgr7YF7tH3A201EqoaVPerM",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: ["oUbJj6GzUToGXJNkbrsiDhGsPZLw","oUbJj6OfvjYV1DbZpWesjHnN1JoE"],
      
